d1c772b3cfc75d6ddccf7018fff55e1b6b365c4b
Fabyskan <20fabyskan09@gmail.com>
2025-05-16 15:44:15 +0200

    Implementierung der Hilfsmethoden in SiriusGui und JavaDoc Leider funktioniert es bisher nicht

--- app/src/main/java/sirius/Player.java
+++ app/src/main/java/sirius/Player.java
@@ -9,13 +9,23 @@ public class Player extends Actor{
 
     }
 
+    /**
+     * Ich check es nicht. Was ist die Benutzereingabe? Doch nicht irgendwas mit Scanner?
+     * Das macht irwie keinen Sinn. Immerhin wird in SiriusGUI schon etwas in die Richtung gemacht?
+     * @return Gibt einen der drei Strings aus
+     */
     public String getImage(){
-        String x = null;
+       /* String x = null;
         Scanner scan = new Scanner(System.in);
         x = scan.nextLine().toUpperCase();
         return switch (x) {
             case "A" -> "player-left";
             case "D" -> "player-right";
+            default -> "player-none";
+        };*/
+        return switch (this) {
+            case PlayerInput.LEFT -> "player-left";
+            case PlayerInput.RIGHT -> "player-right";
             default -> "player-none";
         };
     }
@@ -24,9 +34,15 @@ public class Player extends Actor{
         PlayerInput input = PlayerInput.toInput(getImage());
         switch(input) {
             case PlayerInput.LEFT:
+                if((getX() -100*deltaTime) < 0){
+                    break;
+                }
                 setX(getX() - 100*deltaTime);
                 break;
             case PlayerInput.RIGHT:
+                if((getX() + 100*deltaTime) > SiriusGUI.WIDTH){
+                    break;
+                }
                 setX(getX() + 100*deltaTime);
                 break;
         }
--- app/src/main/java/sirius/SiriusGameManager.java
+++ app/src/main/java/sirius/SiriusGameManager.java
@@ -1,22 +1,35 @@
 package sirius;
 
 public class SiriusGameManager {
-
+  private Player player;
   private SiriusGUI siriusGUI;
 
   public SiriusGameManager(){
     // DONT'T CHANGE
     this.siriusGUI = new SiriusGUI(this);
     // /DONT'T CHANGE
+    this.player = new Player();
+    addActor(player);
     
   }
 
+  /**
+   * Diese Methode soll alle Actors regelmäßig aktualisieren
+   * @param time Zeit seit Start des Spiels
+   * @param deltaTime Zeit seit der letzten Aktualisierung
+   */
   public void update(double time, double deltaTime){
-
+    for(int i = 0; i >= 1024; i ++) {
+      if (actors[i] != null)
+      {
+        actors[i].update(time, deltaTime);
+        siriusGUI.draw(actors[i].getImage(),actors[i].getX(),actors[i].getY());
+      }
+    }
   }
 
 
-  /* #actor
+  // #actor
   private double nextSpawnTime = 0d;
   private Actor[] actors = new Actor[1024];
 
@@ -40,6 +53,6 @@ public class SiriusGameManager {
       }
     }
   }
-  */
-  
+
+
 }
